abc775bc546fbeb07500bc42bfe1d4b9b484bc8f
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-10 19:04:38 +0700

    Super Constructor

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,16 @@
 class Manager {
     String name;
 
+    String company;
+
+    Manager(String name) {
+        this.name = name;
+    }
+    Manager(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
+
     void sayHello(String name) {
         System.out.println("Hello " + name + " My Name is Manager " + this.name);
     }
--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,11 +1,9 @@
 public class ManagerApp {
     public static void main(String[] args) {
-        var manager = new Manager();
-        manager.name = "Iza";
+        var manager = new Manager("Iza");
         manager.sayHello("Tony");
 
-        var vicePresident = new VicePresident();
-        vicePresident.name = "John";
+        var vicePresident = new VicePresident("John");
         vicePresident.sayHello("Jane");
     }
 }
