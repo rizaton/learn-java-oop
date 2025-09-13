f8f9c7e095e22dc838be5e18fa191397600c8600
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-13 11:50:03 +0700

    Package

---
+++ src/coding/rizaton/data/Product.java
@@ -0,0 +1,11 @@
+package coding.rizaton.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
