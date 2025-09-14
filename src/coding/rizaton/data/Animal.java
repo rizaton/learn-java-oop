a24a3031fbf1da9fc96f97bcaae4446beb1f0826
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-14 19:55:08 +0700

    Abstract Method

---
+++ src/coding/rizaton/data/Animal.java
@@ -0,0 +1,6 @@
+package coding.rizaton.data;
+
+public abstract class Animal {
+    public String name;
+    public abstract void run();
+}
---
+++ src/coding/rizaton/data/AnimalApp.java
@@ -0,0 +1,9 @@
+package coding.rizaton.data;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+        Animal animal = new Cat();
+        animal.name = "Puss";
+        animal.run();
+    }
+}
