f51ec97134dce8323a13da181d654d29f2e7a35d
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-15 13:40:40 +0700

    Inner Class

---
+++ src/coding/rizaton/data/Company.java
@@ -0,0 +1,29 @@
+package coding.rizaton.data;
+
+public class Company {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public class Employee {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getCompany(){
+            return Company.this.name;
+        }
+    }
+}
---
+++ src/coding/rizaton/data/CompanyApp.java
@@ -0,0 +1,22 @@
+package coding.rizaton.data;
+
+public class CompanyApp {
+    public static void main(String[] args) {
+
+        Company company = new Company();
+        company.setName("Gubuk Santai");
+
+        Company.Employee employee = company.new Employee();
+        employee.setName("Iza");
+        System.out.println(employee.getName());
+        System.out.println(employee.getCompany());
+
+        Company company2 = new Company();
+        company2.setName("Not yet defined");
+
+        Company.Employee employee2 = company2.new Employee();
+        employee2.setName("Riza");
+        System.out.println(employee2.getName());
+        System.out.println(employee2.getCompany());
+    }
+}
